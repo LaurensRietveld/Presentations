b4e79b65258042cb22e1f3e0beb551ee2cf7e903
Laurens Rietveld <laurens.rietveld@gmail.com>
2015-10-14 15:30:00 +0200

    small fix to pdf export, changed frank logo

--- iswc2015/presentation.js
+++ iswc2015/presentation.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-	
+
 	//draw header stuff. Need to dynamically insert via javascript, because we want the header added once when doing a proper presentation, and we want to add the header to all pages when exporting to pdf
 	var header = '<div class="bottomBar">'+
 		'<img class="vuLogo" src="imgs/VUlogo_EN_Blauw_FC_tcm9-201387.png"</div>'+
@@ -7,11 +7,10 @@ $( document ).ready(function() {
 		'<img class="d2sLogo" src="imgs/elaborate-logo-150dpi.png">'+
 		'</div>';
 	if ( window.location.search.match( /print-pdf/gi ) ) {
-		$('section').append(header);
+		// $('section').append(header);
 	}
 	else {
 		$('div.reveal').append(header);
 	}
 
 });
-
